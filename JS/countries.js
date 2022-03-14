2d8ef24c64eba0cd8af04644d6f7510a2582e417
mb-tousif <meta.block.tousif@gmail.com>
2022-03-14 23:52:19 +0600

    Destructuring of Object

--- JS/countries.js
+++ JS/countries.js
@@ -13,12 +13,38 @@ container.innerHTML = countriesHTML.join(' ');
 // console.log(countriesHTML[78]);
 }
 
-const getCountryHTML= country => {
+// const getCountryHTML= country => {
+//     return `
+//     <div style="background-color: #2ecc71; padding: 20px; margin: 10px;border: 4px solid goldenrod;border-radius: 20px">
+//         <h2>Country Name: ${country.name.common}</h2>
+//         <h2>Capital: ${country.capital}</h2>
+//         <h2>Official Name: ${country.name.official}</h2>
+//         <img src='${country.flags.svg}' style="width: 200px;">
+//     </div>
+//     `;
+// }
+const getCountryHTML= ({ name,capital, flags})=> {
+    // Destructuring Option-two [perameter destructuring]
+    // const { name,capital, flags} = country;
     return `
     <div style="background-color: #2ecc71; padding: 20px; margin: 10px;border: 4px solid goldenrod;border-radius: 20px">
-        <h2>${country.name.common}</h2>
-        <img src='${country.flags.png}' style="width: 200px;">
+        <h2>Country Name: ${name.common}</h2>
+        <h2>Capital: ${capital}</h2>
+        <h2>Official Name: ${name.official}</h2>
+        <img src='${flags.svg}' style="width: 200px;">
     </div>
     `;
 }
+// const getCountryHTML= country => {
+//     // Destructuring Option-one
+//     const { name,capital, flags} = country;
+//     return `
+//     <div style="background-color: #2ecc71; padding: 20px; margin: 10px;border: 4px solid goldenrod;border-radius: 20px">
+//         <h2>Country Name: ${name.common}</h2>
+//         <h2>Capital: ${capital}</h2>
+//         <h2>Official Name: ${name.official}</h2>
+//         <img src='${flags.svg}' style="width: 200px;">
+//     </div>
+//     `;
+// }
 loadCountries();
